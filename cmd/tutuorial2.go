0bb6bcd674cbfacb885655a693ac905191f19277
shivanant <shivanantbhgat7@gmail.com>
2025-08-05 17:39:10 +0530

    ci : Done tutorial 2

---
+++ cmd/tutuorial2.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Welcome to Tutorial 2 !")
+
+	var num1 int16 = 333;
+	fmt.Println("num1: ",num1)
+
+	num2 := 434.434333
+	fmt.Println("num2: ",num2)
+}
