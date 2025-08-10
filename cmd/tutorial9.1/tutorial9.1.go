4ef79f59b713bff21c05f8911607c01210d0a122
shivanant <shivanantbhagat7@gmail.com>
2025-08-10 18:38:15 +0530

    ci : multiple  channels

--- cmd/tutorial9.1/tutorial9.1.go
+++ cmd/tutorial9.1/tutorial9.1.go
@@ -7,14 +7,17 @@ import (
 )
 
 const maxVideo float32= 6
+const maxVideo2 float32= 3
 
 func main() {
 	websites := []string{"google.com", "youtube.com", "duckduckgo.com"}
 	goChannel := make(chan string)
+	goChannel2 := make(chan string)
 	for i:=  range websites{
 		go findVideo(websites[i], goChannel)
+		go findVideo2(websites[i], goChannel2)
 	}
-	fmt.Println(<-goChannel)
+	sendMessage(goChannel,goChannel2)
 	fmt.Println("This is the end of tutorial 9.1 !")
 }
 
@@ -26,5 +29,25 @@ func findVideo( website string, c chan string){
 			c<-website
 			break
 		}
+	}
+}
+func findVideo2( website string, c chan string){
+	for{
+		time.Sleep(time.Second)
+		var video = rand.Float32()*20
+		if video<= maxVideo2{
+			c<-website
+			break
+		}
+	}
+}
+
+func sendMessage(c1 chan string, c2 chan string){
+	select{
+	case website := <-c1:
+		fmt.Println("the website is from chan1 :", website)
+		case website := <-c2:
+		fmt.Println("the website is from chan2 :", website)
+	
 	}
 }
